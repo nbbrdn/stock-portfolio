c1576ac9f487e1ac11f8caf8bd114b1adc8f4136
Nikolay Bayborodin <nbbrdn@gmail.com>
2022-11-14 21:10:51 +1000

    Implement base routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, escape
 
 app = Flask(__name__)
 
@@ -6,3 +6,23 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello World!'
+
+
+@app.route('/about')
+def about():
+    return '<h2>About this application...</h2>'
+
+
+@app.route('/stocks/')
+def stocks():
+    return '<h2>Stocks List</h2>'
+
+
+@app.route('/hello/<message>')
+def hello_message(message):
+    return f'<h1>Welcome {escape(message)}!</h1>'
+
+
+@app.route('/blog-posts/<int:post_id>')
+def display_blog_post(post_id):
+    return f'<h1>Blog Post #{post_id}...</h1>'
