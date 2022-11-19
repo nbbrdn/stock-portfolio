e004a39ab1bf25bc0fc4d80111de76722749582b
Nikolay Bayborodin <nbbrdn@gmail.com>
2022-11-19 19:09:29 +1000

    Add the form functional tests

--- tests/functional/test_stocks.py
+++ tests/functional/test_stocks.py
@@ -47,3 +47,25 @@ def test_get_add_stock_page():
         assert b'Stock Symbol <em>(required)</em>' in response.data
         assert b'Number of Shares <em>(required)</em>' in response.data
         assert b'Purchase Price ($) <em>(required)</em>' in response.data
+
+
+def test_post_add_stock_page():
+    """
+    GIVEN a Flask application
+    WHEN the '/add_stock' page is posted to (POST)
+    THEN check that the user is redirected to the '/list_stocks' page
+    """
+    with app.test_client() as client:
+        response = client.post('/add_stock',
+                                data={'stock_symbol': 'AAPL',
+                                       'number_of_shares': '23',
+                                       'purchase_price': '432.17'},
+                                follow_redirects=True)
+        assert response.status_code == 200
+        assert b'List of Stocks' in response.data
+        assert b'Stock Symbol' in response.data
+        assert b'Number of Shares' in response.data
+        assert b'Purchase Price' in response.data
+        assert b'AAPL' in response.data
+        assert b'23' in response.data
+        assert b'432.17' in response.data
