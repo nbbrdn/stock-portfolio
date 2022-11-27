ac6d9799a5ea03de0fec674f5af1714e32331e3e
Nikolay Bayborodin <nbbrdn@gmail.com>
2022-11-27 22:59:04 +1000

    Reorganize the project usuing blueprints

--- app.py
+++ app.py
@@ -15,20 +15,6 @@ from logging.handlers import RotatingFileHandler
 from pydantic import BaseModel, validator
 
 
-class StockModel(BaseModel):
-    """Class for parsing new stock data from a form."""
-
-    stock_symbol: str
-    number_of_shares: int
-    purchase_price: float
-
-    @validator('stock_symbol')
-    def stock_symbol_check(cls, value):
-        if not value.isalpha() or len(value) > 5:
-            raise ValueError('Stock symbol must be 1-5 characters')
-        return value.upper()
-
-
 app = Flask(__name__)
 
 app.logger.removeHandler(default_handler)
@@ -46,12 +32,6 @@ app.logger.addHandler(file_handler)
 app.secret_key = 'BAD_SECRET_KEY'
 
 app.logger.info('Starting the Stock Portfolio App...')
-
-
-@app.route('/')
-def index():
-    app.logger.info('Calling the index() function.')
-    return render_template('index.html')
 
 
 @app.route('/about')
@@ -74,34 +54,3 @@ def hello_message(message):
 @app.route('/blog-posts/<int:post_id>')
 def display_blog_post(post_id):
     return f'<h1>Blog Post #{post_id}...</h1>'
-
-
-@app.route('/add_stock', methods=['GET', 'POST'])
-def add_stock():
-    if request.method == 'POST':
-        for key, value in request.form.items():
-            print(f'{key}: {value}')
-
-        try:
-            stock_data = StockModel(
-                stock_symbol=request.form['stock_symbol'],
-                number_of_shares=request.form['number_of_shares'],
-                purchase_price=request.form['purchase_price'],
-            )
-            print(stock_data)
-
-            # Save the form data to the session object
-            session['stock_symbol'] = stock_data.stock_symbol
-            session['number_of_shares'] = stock_data.number_of_shares
-            session['purchase_price'] = stock_data.purchase_price
-
-            flash(f'Added new stock ({stock_data.stock_symbol})!', 'success')
-            app.logger.info(
-                f"Added new stock ({request.form['stock_symbol']})"
-            )
-
-            return redirect(url_for('list_stocks'))
-        except ValueError as e:
-            print(e)
-
-    return render_template('add_stock.html')
