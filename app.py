8ee8cd66116bbd83d39d3bbeaf0252a17787c282
Nikolay Bayborodin <nbbrdn@gmail.com>
2022-12-03 17:46:00 +1000

    Reorganize the project to use the application factory pattern

--- app.py
+++ app.py
@@ -1,34 +1,3 @@
-import logging
-import os
+from project import create_app
 
-from flask import Flask
-from flask.logging import default_handler
-from logging.handlers import RotatingFileHandler
-
-
-app = Flask(__name__)
-
-# Configure the Flask application
-config_type = os.getenv('CONFIG_TYPE', default='config.DevelopmentConfig')
-app.config.from_object(config_type)
-
-app.logger.removeHandler(default_handler)
-
-file_handler = RotatingFileHandler(
-    'instance/stock-portfolio.log', maxBytes=16384, backupCount=20
-)
-file_formatter = logging.Formatter(
-    '%(asctime)s %(levelname)s: %(message)s [in %(filename)s:%(lineno)d]'
-)
-file_handler.setFormatter(file_formatter)
-file_handler.setLevel(logging.INFO)
-app.logger.addHandler(file_handler)
-
-
-from project.stocks import stocks_blueprint
-from project.users import users_blueprint
-
-app.register_blueprint(stocks_blueprint)
-app.register_blueprint(users_blueprint, url_prefix='/users')
-
-app.logger.info('Starting the Stock Portfolio App...')
+app = create_app()
---
+++ project/__init__.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from logging.handlers import RotatingFileHandler
+import logging
+from flask.logging import default_handler
+import os
+
+
+def create_app():
+    # Create the Flask aplication
+    app = Flask(__name__)
+
+    # Configure the Flask application
+    config_type = os.getenv('CONFIG_TYPE', default='config.DevelopmentConfig')
+    app.config.from_object(config_type)
+
+    register_blueprints(app)
+    configure_logging(app)
+
+    return app
+
+
+def register_blueprints(app):
+    # Import the blueprints
+    from project.stocks import stocks_blueprint
+    from project.users import users_blueprint
+
+    app.register_blueprint(stocks_blueprint)
+    app.register_blueprint(users_blueprint, url_prefix='/users')
+
+
+def configure_logging(app):
+    # Logging Configuration
+    file_handler = RotatingFileHandler(
+        'instance/stock-portfolio.log', maxBytes=16384, backupCount=20
+    )
+    file_formatter = logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(filename)s:%(lineno)d]'
+    )
+    file_handler.setFormatter(file_formatter)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.removeHandler(default_handler)
+
+    app.logger.info('Starting the Stock Portfolio App...')
