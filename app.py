6a38a6c37b77c262cfeddc899789acd051c6e4dc
Nikolay Bayborodin <nbbrdn@gmail.com>
2024-01-10 02:54:32 +1000

    Implement functional tests

--- app.py
+++ app.py
@@ -29,8 +29,7 @@ def index():
 
 @app.route("/about")
 def about():
-    # return render_template("about.html", company_name="Bayborodin and co.")
-    return render_template("about.html")
+    return render_template("about.html", company_name="Bayborodin and co.")
 
 
 @app.route("/hello/<message>")
